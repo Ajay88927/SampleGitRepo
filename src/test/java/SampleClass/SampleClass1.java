b98494e38360dc2898df5fc1ff1ba47186cef4cf
Ajay88927 <ajayb015@gmail.com>
2021-08-30 12:16:24 +0530

    First commit

---
+++ src/test/java/SampleClass/SampleClass1.java
@@ -0,0 +1,10 @@
+package SampleClass;
+
+public class SampleClass1 {
+
+	public static void main(String[] args) {
+		System.out.println("Class 1 to git");
+
+	}
+
+}
---
+++ src/test/java/SampleClass/SampleClass2.java
@@ -0,0 +1,10 @@
+package SampleClass;
+
+public class SampleClass2 {
+
+	public static void main(String[] args) {
+		System.out.println("Class 2 to git");
+
+	}
+
+}
